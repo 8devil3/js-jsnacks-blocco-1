5c4e4aba0350863e09e1394b569896b533fa89af
8devil3 <8dev.il3@gmail.com>
2022-02-25 14:12:09 +0100

    snack 3

--- js/jsnack-b1.js
+++ js/jsnack-b1.js
@@ -21,36 +21,53 @@ Il software stampa il maggiore. */
 L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga. */
 
-const msgHTML = document.querySelector('#msg');
-const input1 = document.querySelector('#word1');
-const input2 = document.querySelector('#word2');
-const btnCheck = document.querySelector('#check');
+// const msgHTML = document.querySelector('#msg');
+// const input1 = document.querySelector('#word1');
+// const input2 = document.querySelector('#word2');
+// const btnCheck = document.querySelector('#check');
 
-btnCheck.addEventListener('click', checkWords);
+// btnCheck.addEventListener('click', checkWords);
 
-function checkWords() {
-    if (input1.value == '' || input2.value == '') {
+// function checkWords() {
+//     if (input1.value == '' || input2.value == '') {
 
-        msgHTML.innerHTML = 'Inserisci entrambe le parole';
+//         msgHTML.innerHTML = 'Inserisci entrambe le parole';
 
-    } else {
+//     } else {
 
-        if (input1.value.length > input2.value.length) {
+//         if (input1.value.length > input2.value.length) {
 
-            msgHTML.innerHTML = input1.value + ' ' + input2.value;
+//             msgHTML.innerHTML = input1.value + ' ' + input2.value;
 
-        } else if (input1.value.length < input2.value.length) {
+//         } else if (input1.value.length < input2.value.length) {
 
-            msgHTML.innerHTML = input2.value + ' ' + input1.value;
+//             msgHTML.innerHTML = input2.value + ' ' + input1.value;
 
-        } else {
+//         } else {
 
-            msgHTML.innerHTML = 'Le parole hanno la stessa lunghezza pari a ' + input1.value.length + ' caratteri';
-        }
-    }
+//             msgHTML.innerHTML = 'Le parole hanno la stessa lunghezza pari a ' + input1.value.length + ' caratteri';
+//         }
+//     }
+// }
+
+
+
+
+
+/* Snack 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+(provatelo anche senza array) */
+
+let number;
+let sum = 0;
+
+for (let x = 1; x <= 10; x++) {
+    number = parseInt(prompt('Inserisci un numero. Richiesta n. ' + x));
+    sum += number;
 }
 
-
+console.log(sum);
 
 
 
